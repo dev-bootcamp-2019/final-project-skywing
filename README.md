4542990251a19738a8424497c74ad1509e0cdea6
skywing <skywing@users.noreply.github.com>
2019-01-17 11:34:36 -0500

    simple loan test case

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 For Consensys Academy 2018
 
+For testing, you need to install truffle-assertions
+
+npm install --save-dev truffle-assertions
--- test/simpleloan.test.js
+++ test/simpleloan.test.js
@@ -1,42 +1,31 @@
-var LoanUtil = artifacts.require('LoanUtil');
-var SimpleLoan = artifacts.require('SimpleLoan');
+const LoanUtil = artifacts.require('LoanUtil');
+const SimpleLoan = artifacts.require('SimpleLoan');
+const truffleAssert = require('truffle-assertions');
 
-contract('SimpleLoan', function(accounts) {
+const LoanStatus = { 
+    Requesting: 0, 
+    Funding: 1, 
+    Funded: 2, 
+    FundWithdrawn: 3, 
+    Repaid: 4, 
+    Defaulted: 5, 
+    Refunded: 6, 
+    Cancelled: 7, 
+    Disabled: 8 
+};
+
+const emptyAddress = '0x0000000000000000000000000000000000000000';
+
+contract('SimpleLoan-Setup', function(accounts)  {
 
     const owner = accounts[0];
     const borrower = accounts[1];
-    const requestedLoanAmt = "10";
-
-    const lender1 = accounts[3];
-    const lender2 = accounts[4];
-    const lender3 = accounts[5];
-
-    const lendingAmt1 = web3.utils.toWei(web3.utils.toBN(2), "ether");
-    const lendingAmt2 = web3.utils.toWei(web3.utils.toBN(3), "ether");
-    const lendingAmt3 = web3.utils.toWei(web3.utils.toBN(5), "ether");
-
-    const loanLib = LoanUtil.deployed();
-    const simpleLoan = SimpleLoan.new()
-
-    const loanAmount = web3.utils.toWei(requestedLoanAmt, "ether");
-    const emptyAddress = '0x0000000000000000000000000000000000000000';
-    
-    const LoanStatus = { 
-        Requesting: 0, 
-        Funding: 1, 
-        Funded: 2, 
-        FundWithdrawn: 3, 
-        Repaid: 4, 
-        Defaulted: 5, 
-        Refunded: 6, 
-        Cancelled: 7, 
-        Disabled: 8 
-    };
-
-    var loan;
+    const loanAmount = web3.utils.toWei("10", "ether");
+    let loan;
 
     it("should create the contract with borrowing info for funding", async() => {
-        loan = await SimpleLoan.deployed();
+
+        loan =  await SimpleLoan.new();
         assert.equal(await loan.status(), LoanStatus.Requesting, "new created loan not in requesting status.");
 
         await loan.requestLoan(borrower, loanAmount);
@@ -46,7 +35,37 @@ contract('SimpleLoan', function(accounts) {
         assert.equal(loanAmount.toString(), String(await loan.loanAmount()), "Request loan amount not matched.");
     });
 
+    it("should create the contract with borrowing info for funding", async() => {
+        await loan.requestLoan(borrower, loanAmount);
+        assert.equal(await loan.status(), LoanStatus.Funding, "loan with borrowing info should be in funding status.")
+        
+        assert.equal(borrower, await loan.borrower(), "borrower address not matched.");
+        assert.equal(loanAmount.toString(), String(await loan.loanAmount()), "Request loan amount not matched.");
+    });
+}); 
+
+
+contract('SimpleLoan-Fund-Refund', function(accounts) {
+
+    const owner = accounts[0];
+    const borrower = accounts[1];
+
+    const lender1 = accounts[3];
+    const lender2 = accounts[4];
+    const lender3 = accounts[5];
+
+    const lendingAmt1 = web3.utils.toWei(web3.utils.toBN(2), "ether");
+    const lendingAmt2 = web3.utils.toWei(web3.utils.toBN(3), "ether");
+    const lendingAmt3 = web3.utils.toWei(web3.utils.toBN(5), "ether");
+
+    const loanAmount = web3.utils.toWei("10", "ether");
+
+    let loan;
+
     it("should be funded by 3 lenders with the amount of 2, 3, and 5 either", async() => {
+        loan =  await SimpleLoan.new();
+        await loan.requestLoan(borrower, loanAmount);
+
         await loan.depositFund({from: lender1, value: lendingAmt1});
         assert.equal(lendingAmt1.toString(), (await loan.ownedAmount()).toString(), "1st loan amount not match what just funded");
 
@@ -55,29 +74,87 @@ contract('SimpleLoan', function(accounts) {
 
         await loan.depositFund({from: lender3, value: lendingAmt3});
         assert.equal(lendingAmt1.add(lendingAmt2).add(lendingAmt3).toString(), String(await loan.ownedAmount()), "3nd loan amount not match what just funded");
-
         assert.equal((await loan.getBalance()).toString(), loanAmount.toString(), "Balance not matched");
         assert.equal(await loan.status(), LoanStatus.Funded, "Loan status should be funded");
     });
 
     it('should refund all the fund to the lender', async() => {
-        var lender1_initialBalance, lender2_initialBalance, lender3_initialBalance;
 
-        lender1_initialBalance = await web3.eth.getBalance(lender1);
-        lender2_initialBalance = await web3.eth.getBalance(lender2);
-        lender3_initialBalance = await web3.eth.getBalance(lender3);
-
-        expected_lender1_balance = web3.utils.toBN(lender1_initialBalance); //.add(lendingAmt1);
-         //.sub(web3.utils.toBN(await web3.eth.getGasPrice()));
+        expected_lender1_balance = web3.utils.toBN(await web3.eth.getBalance(lender1)).add(lendingAmt1);
+        expected_lender2_balance = web3.utils.toBN(await web3.eth.getBalance(lender2)).add(lendingAmt2);
+        expected_lender3_balance = web3.utils.toBN(await web3.eth.getBalance(lender3)).add(lendingAmt3);
 
         await loan.refund({from:owner});
         assert.equal((await loan.ownedAmount()).toString(), "0", "Expecting owned amount to be 0.");
         assert.equal((await loan.lenderCount()).toString(), "0", "Expecting lender count to be 0.");
         assert.equal((await loan.getBalance()).toString(), "0", "Expecting balance to be 0.");
 
-        
         current_lender1_balance = web3.utils.toBN(await web3.eth.getBalance(lender1));
         assert.isTrue(current_lender1_balance.eq(expected_lender1_balance), 
             "curreunt value:" + current_lender1_balance.toString() + " expected value:" + expected_lender1_balance.toString());
+
+        current_lender2_balance = web3.utils.toBN(await web3.eth.getBalance(lender2));
+            assert.isTrue(current_lender2_balance.eq(expected_lender2_balance), 
+            "curreunt value:" + current_lender2_balance.toString() + " expected value:" + expected_lender2_balance.toString());
+
+        current_lender3_balance = web3.utils.toBN(await web3.eth.getBalance(lender3));
+            assert.isTrue(current_lender3_balance.eq(expected_lender3_balance), 
+            "curreunt value:" + current_lender3_balance.toString() + " expected value:" + expected_lender3_balance.toString());
+
+        assert.equal(await loan.status(), LoanStatus.Refunded, "Loan status should be refunded");
     });
-}); 
+
+    it("should fail because the status is refunded", async() => {
+        await truffleAssert.fails(loan.refund({from:owner}), truffleAssert.ErrorType.REVERT);
+    });
+
+});
+
+
+contract('SimpleLoan-Fund-Witdrawn', function(accounts) {
+    const owner = accounts[0];
+    const borrower = accounts[1];
+
+    const lender1 = accounts[3];
+    const lender2 = accounts[4];
+    const lender3 = accounts[5];
+
+    const lendingAmt1 = web3.utils.toWei(web3.utils.toBN(2), "ether");
+    const lendingAmt2 = web3.utils.toWei(web3.utils.toBN(3), "ether");
+    const lendingAmt3 = web3.utils.toWei(web3.utils.toBN(5), "ether");
+
+    const loanAmount = web3.utils.toWei("10", "ether");
+    const emptyAddress = '0x0000000000000000000000000000000000000000';
+
+    let loan;
+
+    it('should witdrawn all the fund to the borrower', async() => {
+
+        loan = await SimpleLoan.new();
+        await loan.requestLoan(borrower, loanAmount);
+        await loan.depositFund({from: lender1, value: lendingAmt1});
+        await loan.depositFund({from: lender2, value: lendingAmt2});
+        await loan.depositFund({from: lender3, value: lendingAmt3});
+
+        expected_lender1_balance = web3.utils.toBN(await web3.eth.getBalance(lender1)).add(lendingAmt1);
+        expected_lender2_balance = web3.utils.toBN(await web3.eth.getBalance(lender2)).add(lendingAmt2);
+        expected_lender3_balance = web3.utils.toBN(await web3.eth.getBalance(lender3)).add(lendingAmt3);
+
+        await loan.refund({from:owner});
+        assert.equal((await loan.ownedAmount()).toString(), "0", "Expecting owned amount to be 0.");
+        assert.equal((await loan.lenderCount()).toString(), "0", "Expecting lender count to be 0.");
+        assert.equal((await loan.getBalance()).toString(), "0", "Expecting balance to be 0.");
+
+        current_lender1_balance = web3.utils.toBN(await web3.eth.getBalance(lender1));
+        assert.isTrue(current_lender1_balance.eq(expected_lender1_balance), 
+            "curreunt value:" + current_lender1_balance.toString() + " expected value:" + expected_lender1_balance.toString());
+
+
+        assert.equal(await loan.status(), LoanStatus.Refunded, "Loan status should be refunded");
+    });
+
+    it("should fail because the status is withdrawn", async() => {
+        await truffleAssert.fails(loan.withdrawFund({from:borrower}), truffleAssert.ErrorType.REVERT);
+    });
+
+});
