98ef5009c9f392aa85745269a0fa21067b718a3b
skywing <skywing@users.noreply.github.com>
2019-01-25 08:22:42 -0500

    contract details 1

--- client/src/components/loandetails.jsx
+++ client/src/components/loandetails.jsx
@@ -3,25 +3,33 @@ import { Redirect } from 'react-router-dom';
 import SimpleLoanContract from "../contracts/SimpleLoan.json"; 
 
 class LoanDetails extends Component {
-    state = {formTitle: "Contract - "}
+    state = {formTitle: "Contract - ", loan: null}
 
-    componentDidMount = async() => {
+    async componentDidMount() {
+        const { web3 } = this.props;
         this.props.onTitle(this.state.formTitle + this.props.match.params.id);
-        //console.log(this.props.web3);
-               
-        let loanInstance = new this.props.web3.eth.Contract(SimpleLoanContract.abi, this.props.match.params.id);
-        //console.log(loanInstance);
-        //console.log('owner', await loanInstance.methods.owner().call());
-        this.setState(() => ({loan: loanInstance}));
-        console.log(this.state.loan);
+        try {
+            let loanInstance = new web3.eth.Contract(SimpleLoanContract.abi, this.props.match.params.id);
+            console.log(loanInstance);
+            loanInstance.methods.owner().call().then((addr) => { console.log(addr)});
+            this.setState({loan: loanInstance});
+
+            this.setState({test: 'hello world'});
+            //console.log(this.state.test);
+        } catch(error) {
+            console.error(error);
+        }
+        
     }
 
     render() {
         return (
-            <div>
-                <h1>details</h1>
-                
-            </div>
+            <form>
+                <div className="form-group row">
+                    <label htmlFor="borrowerAddr">Borrower</label>
+                    <input type="text" id="borrowerAddr" className="form-control" planceholder={this.state.test}/>
+                </div>
+            </form>
         );
     }
 }
